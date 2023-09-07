b16e49a9041200493064c711321404cfaf935890
Morais453 <137251315+Morais453@users.noreply.github.com>
2023-09-07 20:47:00 -0300

    finalizado falta add limit nas gerações

--- assets/js/Poke-api.js
+++ assets/js/Poke-api.js
@@ -2,7 +2,7 @@
 const pokeApi = {}
 function convertPADTP(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.num = pokeDetail.order
+    pokemon.num = pokeDetail.id
     pokemon.name = pokeDetail.name
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon)  => {
         .then(convertPADTP)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,35 @@
 const PokemonList = document.getElementById("PokemonList") 
+const loadB = document.getElementById('loadB')
+const limit = 10
+let offset = 0;
+
+const maxRecord = 11
 
 
-function convertPokeToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}"> 
-            <span class="number">#${pokemon.num}</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}"
-                    alt='${pokemon.name}'>
-            </div>
-        </li>
-    `
+function PokemonItens (offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {   
+        const NovaL = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}"> 
+                <span class="number">#${pokemon.num}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                        alt='${pokemon.name}'>
+                </div>
+            </li>
+        `).join('')
+        PokemonList.innerHTML += NovaL
+    })    
 }
 
+PokemonItens(offset, limit)
 
-
-pokeApi.getPokemons().then((pokemons = []) => {   
-    PokemonList.innerHTML += pokemons.map(convertPokeToLi).join('')
+loadB.addEventListener('click', () => {
+    offset += limit
+    
+    PokemonItens(offset, limit)
 })
